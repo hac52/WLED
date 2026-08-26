e3e4b1fc0b65fd95abff523ea7a4ed7b19e86190
hac52 <marcel.woelfle@outlook.de>
2026-08-26 03:23:18 +0200

    Delete usermods/SwitchBot directory

--- usermods/SwitchBot/SwitchBotUsermod.cpp
+++
@@ -1,5 +0,0 @@
-#include "SwitchBot_PM1_Cloud.h"
-
-// Registriert den Usermod beim WLED-System, damit er mitkompiliert wird
-static SwitchBot switchbot_usermod;
-REGISTER_USERMOD(switchbot_usermod);
